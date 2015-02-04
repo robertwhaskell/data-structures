0963429e31425db8904fee855bbbeae17d4535ec
Robert Haskell <robertwhaskell@gmail.com>
2015-02-04 11:54:27 -0800

    made skeleton/method for insert(val)

--- linked_list.py
+++ linked_list.py
@@ -9,3 +9,11 @@ class list(object):
         def __init__(self, head):
             super(list, self).__init__()
             self.head = head
+
+        def insert(self, val):
+            #make a new node with the Value
+            newNode = Node(val, self.head)
+            #make its next value equal to the head
+            #make this new node the head
+            self.head = newNode
+        
