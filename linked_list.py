e4c69caad1d376f9b834a12121d5f34b525dcf9a
Robert Haskell <robertwhaskell@gmail.com>
2015-02-04 11:51:29 -0800

    made linked_list.py file, set class structures for node and list

---
+++ linked_list.py
@@ -0,0 +1,11 @@
+class Node(object):
+    def __init__(self, val, next):
+        super(Node, self).__init__()
+        self.val = val
+        self.next = next
+
+
+class list(object):
+        def __init__(self, head):
+            super(list, self).__init__()
+            self.head = head
