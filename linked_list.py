e3808f8c0724fb8627e9ab4c45020f5c4253a0c4
Nicholas Draper <ndraper2@gmail.com>
2015-02-05 13:00:06 -0800

    changing display to use a string instead of a tuple

--- linked_list.py
+++ linked_list.py
@@ -7,14 +7,12 @@ class Node(object):
 class list(object):
     def __init__(self, head=None):
         self.head = head
-        self.list_size = 0
 
     def insert(self, val):
         # make a new node with the Value
         # make its next value equal to the head
         # make this new node the head
         self.head = Node(val, self.head)
-        self.list_size += 1
 
     def pop(self):
         # make a node that has the value of the head
@@ -22,18 +20,16 @@ class list(object):
         if self.head is not None:
             head_val = self.head.val
             self.head = self.head.next
-            self.list_size -= 1
             return head_val
 
     def size(self):
-        iterate through each and add to count.
+        #iterate through each and add to count.
         iter_node = self.head
         count = 0
         while iter_node is not None:
             count += 1
             iter_node = iter_node.next
         return count
-        #return self.list_size
 
     def search(self, val):
         # iterate through the list
@@ -65,12 +61,19 @@ class list(object):
         # iterate though the list
         # add the values of each node
         # to the tuple, then display.
-        tup = ()
+        output_string = ""
         iter_node = self.head
-        while iter_node is not None:
-            tup = tup + (iter_node.val,)
+        while iter_node:
+            if isinstance(iter_node.val, str):
+                output_string += "'{}'"  .format(iter_node.val)
+            else:
+                output_string += "{}" .format(iter_node.val)
             iter_node = iter_node.next
-        return tup
+            if iter_node:
+                output_string += ", "
+            else:
+                return "({})" .format(output_string)
+        return "()"
 
     def __str__(self):
         return str(self.display())
