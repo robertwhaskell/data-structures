cc82106f566b6f936d00ff916cdbd83326fdc50c
Nicholas Draper <ndraper2@gmail.com>
2015-02-07 20:22:50 -0800

    added best case for broken return, changed asserts to be one line instead of two

--- test_nickparen.py
+++ test_nickparen.py
@@ -1,10 +1,13 @@
 def test_best_case_balanced():
     from nickparen import check_parentheses
-    val = check_parentheses(u"()")
-    assert val == 0
+    assert check_parentheses(u'()') == 0
 
 
 def test_best_case_open():
     from nickparen import check_parentheses
-    val = check_parentheses(u"((")
-    assert val == 1
+    assert check_parentheses(u'((') == 1
+
+
+def test_best_case_broken():
+    from nickparen import check_parentheses
+    assert check_parentheses(u')(') == -1
