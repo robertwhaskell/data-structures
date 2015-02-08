816e08c82a3b37d2dbbc23b2b2861b1012182021
Nicholas Draper <ndraper2@gmail.com>
2015-02-07 20:46:30 -0800

    added tests with other symbols in the string

--- test_nickparen.py
+++ test_nickparen.py
@@ -25,3 +25,16 @@ def test_not_unicode():
     from nickparen import check_parentheses
     with pytest.raises(TypeError):
         check_parentheses(5)
+
+def test_no_parentheses():
+    from nickparen import check_parentheses
+    assert check_parentheses(u"foo") == 0
+
+def test_symbols_string_unicode():
+    from nickparen import check_parentheses
+    assert check_parentheses(u"@!%^'(&^$*") == 1
+
+
+def test_symbols_string_bytestring():
+    from nickparen import check_parentheses
+    assert check_parentheses("!#@$&^*)") == -1
