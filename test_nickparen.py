7edaa1636e2c20d813a9151a2d27b42a90f1c679
Nicholas Draper <ndraper2@gmail.com>
2015-02-07 20:19:09 -0800

    implemented best case test for open return

--- test_nickparen.py
+++ test_nickparen.py
@@ -2,3 +2,9 @@ def test_best_case_balanced():
     from nickparen import check_parentheses
     val = check_parentheses(u"()")
     assert val == 0
+
+
+def test_best_case_open():
+    from nickparen import check_parentheses
+    val = check_parentheses(u"((")
+    assert val == 1
