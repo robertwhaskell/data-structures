fdbdab3d14c2141fd77e57b3bbe98cf2993f92c5
Robert Haskell <robertwhaskell@gmail.com>
2015-02-10 13:23:49 -0800

    added working functionality for insert, append, and remove(almost)

--- doubly_linked.py
+++ doubly_linked.py
@@ -5,20 +5,36 @@ class DL_Node(object):
         self.prev = prev
 
 
-
 class DL_List(object):
-    def __init__(self, hea=None, tail=None):
+    def __init__(self, head=None, tail=None):
         self.head = head
         self.tail = tail
 
-    def insert(val):
-        pass
+    def insert(self, val):
+        if self.head:
+            self.head.next = DL_Node(val, None, self.head)
+            self.head = self.head.next
+        else:
+            self.head = self.tail = DL_Node(val)
 
-    def append(val):
-        pass
+    def append(self, val):
+        if self.tail:
+            self.tail.prev = DL_Node(val, self.tail, None)
+            self.tail = self.tail.prev
+        else:
+            self.tail = self.head = DL_Node(val)
 
-    def remove(val):
-        pass
+    def remove(self, val):
+        iter_node = self.head
+        while iter_node:
+            if iter_node.val == val:
+                if iter_node.next:
+                    iter_node.next.prev = iter_node.prev
+                if iter_node.prev:
+                    iter_node.prev.next = iter_node.next
+                return
+            iter_node = iter_node.next
+        raise AttributeError("Value not found")
 
     def pop():
         pass
--- test_doubly_linked.py
+++ test_doubly_linked.py
@@ -1,28 +1,53 @@
 import pytest
+from doubly_linked import DL_List
+from doubly_linked import DL_Node
+
+
+@pytest.fixture
+def pop_list(request):
+    l = DL_List()
+    l.insert(5)
+    l.insert(True)
+    l.insert("Hello")
+    l.insert("Yay")
+    l.insert(False)
+    return l
+
 
 def test_doubly_linked_list(request):
-    pass
+    test_node = DL_Node(5)
+    assert test_node.val == 5
+    assert test_node.next is None
+    assert test_node.prev is None
 
 
 def test_doubly_linked_node(request):
-    pass
+    test_list = DL_List()
+    assert test_list.head is None
+    assert test_list.tail is None
 
 
-def test_insert(request):
-    pass
+def test_insert(request, pop_list):
+    pop_list.insert("New Val")
+    assert pop_list.head.val == "New Val"
 
 
-def test_append(request):
-    pass
+def test_append(request, pop_list):
+    pop_list.append("New Val")
+    assert pop_list.tail.val == "New Val"
 
 
-def test_pop(request):
-    pass
+def test_pop(request, pop_list):
+    assert pop_list.pop() == 5
+    assert pop_list.head.val is True
 
 
-def test_shift(request):
-    pass
+def test_shift(request, pop_list):
+    assert pop_list.shift() is False
+    assert pop_list.tail.val == "Yay"
 
 
-def test_remove(request):
-    pass
+def test_remove(request, pop_list):
+    pop_list.remove("Hello")
+    with pytest.raises(AttributeError):
+        pop_list.remove("Hello")
