7ffa6bd93a814a76fdcee5d59eabf9c859924a56
Robert Haskell <robertwhaskell@gmail.com>
2015-02-10 12:46:09 -0800

    created skeletons for testing and for classes.

---
+++ doubly_linked.py
@@ -0,0 +1,31 @@
+class DL_Node(object):
+    def __init__(self, val, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+
+
+class DL_List(object):
+    def __init__(self, hea=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def insert(val):
+        pass
+
+    def append(val):
+        pass
+
+    def remove(val):
+        pass
+
+    def pop():
+        pass
+
+    def shift():
+        pass
+
+
+
+
---
+++ test_doubly_linked.py
@@ -0,0 +1,28 @@
+import pytest
+
+def test_doubly_linked_list(request):
+    pass
+
+
+def test_doubly_linked_node(request):
+    pass
+
+
+def test_insert(request):
+    pass
+
+
+def test_append(request):
+    pass
+
+
+def test_pop(request):
+    pass
+
+
+def test_shift(request):
+    pass
+
+
+def test_remove(request):
+    pass
