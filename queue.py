a47bf5e60fcf0ddbfa04af23342bbc15b2699efa
Robert Haskell <robertwhaskell@gmail.com>
2015-02-09 17:19:34 -0800

    added a size() method, and appropriate tests

--- queue.py
+++ queue.py
@@ -8,7 +8,7 @@ class Queue(object):
     def __init__(self):
         self.head = None
         self.tail = None
-        self.size = 0
+        self.list_size = 0
 
     def enqueue(self, val=None):
         if self.tail:
@@ -16,13 +16,16 @@ class Queue(object):
             self.tail = self.tail.next
         else:
             self.tail = self.head = Node(val)
-        self.size += 1
+        self.list_size += 1
 
     def dequeue(self):
         try:
             head_val = self.head.val
             self.head = self.head.next
-            self.size -= 1
+            self.list_size -= 1
             return head_val
         except AttributeError:
             raise AttributeError("Empty Queue")
+
+    def size(self):
+        return self.list_size
--- test_queue.py
+++ test_queue.py
@@ -12,20 +12,25 @@ def populated_queue(request):
     return q
 
 
+def test_Queue(request):
+    assert Queue().head == Queue().tail
+    assert Queue().size() == 0
+
+
 def test_enqueue(request):
     q = Queue()
     q.enqueue(5)
     assert q.head.val == q.tail.val
     assert q.head.val == 5
-    assert q.size == 1
+    assert q.size() == 1
 
 
 def test_dequeue(request, populated_queue):
     assert populated_queue.dequeue() == 5
     assert populated_queue.head.val == "True"
-    assert populated_queue.size == 3
+    assert populated_queue.size() == 3
 
 
 def test_size(request, populated_queue):
-    assert populated_queue.size == 4
-    assert Queue().size == 0
+    assert populated_queue.size() == 4
+    assert Queue().size() == 0
