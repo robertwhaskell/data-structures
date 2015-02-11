a2d262a79a929c797fe4cf87cda85e5e8bf08134
Robert Haskell <robertwhaskell@gmail.com>
2015-02-11 14:41:33 -0800

    Wrote a crazy amount of recursive code

--- binary_heap.py
+++ binary_heap.py
@@ -11,17 +11,62 @@ class Heap(object):
         self.head = None
 
     def push(self, val):
-        self.find_open_spot(self.head, val)
+        placed_node = self.find_open_spot(self.head, val)
+        holder = placed_node.val
+        try:
+            while holder > placed_node.parent.val:
+                placed_node.val = placed_node.parent.val
+                placed_node.parent.val = holder
+                placed_node = placed_node.parent
+        except AttributeError:
+            pass
 
     def pop(self):
+        self.head.val = self.find_and_destroy_last_node(self.head)
+        self.check_values(self.head, self.head.val)
         pass
 
-    def find_open_spot(self, iter_node, val):
-        if not iter_node.left_child:
-            iter_node.left_child = Node(val)
-        else:
-            self.find_open_spot(iter_node.left_child, val)
-        if not iter_node.right_child:
-            iter_node.right_child = Node(val)
-        else:
-            self.find_open_spot(iter_node.right_child, val)
+    def check_values(self, iter_node, val):
+        try:
+            if iter_node.left_child.val > val:
+                iter_node.val = iter_node.left_child.val
+                iter_node.left_child.val = val
+                self.check_values(iter_node.left_child, iter_node.left_child.val)
+
+            if iter_node.right_child.val > val:
+                iter_node.val = iter_node.right_child.val
+                iter_node.right_child.val = val
+                self.check_values(iter_node.right_child, iter_node.right_child.val)
+        except AttributeError:
+            return
+
+    def has_right_sibling(self, iter_node):
+        try:
+            return iter_node.parent.left_child
+        except AttributeError:
+            return False
+
+    def find_and_destroy_last_node(self, iter_node):
+        try:
+            self.find_and_destroy_last_node(iter_node.left_child)
+            self.find_and_destroy_last_node(iter_node.right_child)
+            self.find_and_destroy_last_node(self.has_right_sibling(iter_node))
+            iter_val = iter_node.val
+            iter_node = None
+            return iter_val
+        except AttributeError:
+            return
+
+    def find_open_spot(self, iter_parent, iter_node, val):
+        try:
+            if iter_node.left_child:
+                if iter_node.right_child:
+                    if self.has_right_sibling(iter_node):
+                        self.find_open_spot(iter_node.parent, iter_node.parent.right_child, val)
+                else:
+                    self.find_open_spot(iter_node, iter_node.right_child, val)
+            else:
+                self.find_open_spot(iter_node, iter_node.left_child, val)
+        except AttributeError:
+            iter_node = Node(val, iter_parent)
+            return True
