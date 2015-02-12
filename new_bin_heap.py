6943af53f121b9d4a0572b753495bb9192705035
Nicholas Draper <ndraper2@gmail.com>
2015-02-11 21:56:50 -0800

    implemented pop and bubble down

--- new_bin_heap.py
+++ new_bin_heap.py
@@ -16,3 +16,28 @@ class Heap:
                 self.heapList[(i-1) // 2] = self.heapList[i]
                 self.heapList[i] = temp
             i = (i - 1) // 2
+
+    def pop(self):
+        val = self.heapList[0]
+        self.heapList[0] = self.heapList[self.size -1]
+        self.size -= 1
+        self.heapList.pop()
+        self.sort_down(0)
+
+    def sort_down(self, i):
+        while ((i + 1) * 2) <= self.size:
+            max_child = self.maxChild(i)
+            if self.heapList[i] < self.heapList[max_child]:
+                temp = self.heapList[i]
+                self.heapList[i] = self.heapList[max_child]
+                self.heapList[max_child] = temp
+            i = max_child
+
+    def maxChild(self, i):
+        if (i * 2 + 2) > self.size:
+            return (i * 2 + 1)
+        else:
+            if self.heapList[i*2 + 1] > self.heapList[i * 2 + 2]:
+                return (i * 2 + 1)
+            else:
+                return (i * 2 + 2)
