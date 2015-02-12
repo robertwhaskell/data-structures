e511e7b8813248760c3df24c15a95098010b42f8
Nicholas Draper <ndraper2@gmail.com>
2015-02-11 22:31:41 -0800

    implemented constructor tests for new binary heap

--- new_bin_heap.py
+++ new_bin_heap.py
@@ -1,7 +1,14 @@
 class Heap:
-    def __init__(self):
-        self.heapList = []
+    def __init__(self, new_list=None):
+        self.heapList = [0]  # root 0 simplifies math
         self.size = 0
+        if new_list:
+            i = len(new_list) // 2
+            self.size = len(new_list)
+            self.heapList += new_list[:]
+            while (i > 0):
+                self.sort_down(i)
+                i -= 1
 
     def push(self, val):
         self.heapList.append(val)
@@ -9,24 +16,26 @@ class Heap:
         self.sort_up(self.size)
 
     def sort_up(self, i):
-        while (i-1) // 2 > 0:
+        while i // 2 > 0:
             # this line will need altered to handle user spec
-            if self.heapList[i] > self.heapList[(i-1) // 2]:
-                temp = self.heapList[(i - 1) // 2]
-                self.heapList[(i-1) // 2] = self.heapList[i]
+            if self.heapList[i] > self.heapList[i // 2]:
+                temp = self.heapList[i // 2]
+                self.heapList[i // 2] = self.heapList[i]
                 self.heapList[i] = temp
-            i = (i - 1) // 2
+            i = i // 2
 
     def pop(self):
-        val = self.heapList[0]
-        self.heapList[0] = self.heapList[self.size -1]
+        val = self.heapList[1]
+        self.heapList[1] = self.heapList[self.size]
         self.size -= 1
         self.heapList.pop()
-        self.sort_down(0)
+        self.sort_down(1)
+        return val
 
     def sort_down(self, i):
-        while ((i + 1) * 2) <= self.size:
+        while (i * 2) <= self.size:
             max_child = self.maxChild(i)
+            # user spec change
             if self.heapList[i] < self.heapList[max_child]:
                 temp = self.heapList[i]
                 self.heapList[i] = self.heapList[max_child]
@@ -34,10 +43,9 @@ class Heap:
             i = max_child
 
     def maxChild(self, i):
-        if (i * 2 + 2) > self.size:
-            return (i * 2 + 1)
-        else:
-            if self.heapList[i*2 + 1] > self.heapList[i * 2 + 2]:
-                return (i * 2 + 1)
-            else:
-                return (i * 2 + 2)
+        if (i * 2 + 1) > self.size:
+            return (i * 2)
+        # user spec change
+        if self.heapList[i * 2] > self.heapList[i * 2 + 1]:
+            return (i * 2)
+        return (i * 2 + 1)
---
+++ test_new_bin_heap.py
@@ -0,0 +1,41 @@
+from new_bin_heap import Heap
+import pytest
+
+
+@pytest.fixture
+def empty_heap():
+    h = Heap()
+    return h
+
+
+@pytest.fixture
+def full_heap():
+    h = Heap()
+    h.push(100)
+    h.push(19)
+    h.push(36)
+    h.push(17)
+    h.push(3)
+    h.push(25)
+    h.push(1)
+    h.push(2)
+    h.push(7)
+    return h
+
+
+def test_heap():
+    h = Heap()
+    assert h.heapList[0] == 0
+    assert h.size == 0
+
+
+def test_iter_heap():
+    l = [100, 19, 36, 17, 3, 25, 1, 2, 7]
+    h = Heap(l)
+    assert h.heapList[1] == 100
+    assert h.size == 9
+
+
+def test_pop(full_heap):
+    assert full_heap.pop() == 100
+    assert full_heap.pop() == 36
