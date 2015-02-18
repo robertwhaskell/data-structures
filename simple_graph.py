9e39c318297e3dcb815bfb10202989748ae17334
Robert Haskell <robertwhaskell@gmail.com>
2015-02-18 12:59:46 -0800

    set up skeleton for Graph()

---
+++ simple_graph.py
@@ -0,0 +1,32 @@
+class Graph(object):
+
+    def __init__(self):
+        self.graph == {}
+
+    def nodes(self):
+        pass
+
+    def edges(self):
+        pass
+
+    def add_node(self, node):
+        self.graph.setdefault(node, [])
+        pass
+
+    def add_edge(self, node1, node2):
+        pass
+
+    def del_node(self, node1):
+        pass
+
+    def del_edge(self, node1, node2):
+        pass
+
+    def has_node(self, node):
+        pass
+
+    def neighbors(self, node):
+        pass
+
+    def adjacent(self, node1, node2):
+        pass
