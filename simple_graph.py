72dfb98e77a965e95c4e5dc6a574c3dfb1227244
Nicholas Draper <ndraper2@gmail.com>
2015-02-18 21:14:20 -0800

    added docstrings to simple_graph

--- simple_graph.py
+++ simple_graph.py
@@ -4,9 +4,11 @@ class Graph(object):
         self.graph = {}
 
     def nodes(self):
+        """Return a list of nodes in the graph."""
         return self.graph.keys()
 
     def edges(self):
+        """Return a list of edges in the graph."""
         l = []
         for k, v in self.graph.iteritems():
             for node in v:
@@ -14,9 +16,11 @@ class Graph(object):
         return l
 
     def add_node(self, node):
+        """Add a node to the graph."""
         self.graph.setdefault(node, [])
 
     def add_edge(self, node1, node2):
+        """Add an edge to the graph."""
         try:
             self.graph.setdefault(node2, [])
             self.graph[node1].append(node2)
@@ -24,6 +28,8 @@ class Graph(object):
             self.graph.setdefault(node1, [node2])
 
     def del_node(self, node):
+        """Delete a node from the graph. Throw a KeyError if the node wasn't
+        in the graph."""
         try:
             self.graph.pop(node)
         except KeyError:
@@ -33,6 +39,9 @@ class Graph(object):
                 v.remove(node)
 
     def del_edge(self, node1, node2):
+        """Delete an edge from the graph. Throw a KeyError if the first node
+         wasn't in the graph.  Throw a ValueError if the second node wasn't
+         in the graph."""
         temp = ''
         try:
             temp = self.graph[node1]
@@ -44,31 +53,40 @@ class Graph(object):
             raise ValueError("{} does not exist".format(node2))
 
     def has_node(self, node):
+        """Return True if node is in graph."""
         return node in self.graph
 
     def neighbors(self, node):
+        """Return a list of nodes connected to the supplied node. Throw a
+         KeyError if the supplied node wasn't in the graph."""
         try:
             return self.graph[node]
         except KeyError:
             raise KeyError("{} doesn't exist".format(node))
 
     def adjacent(self, node1, node2):
+        """Return true if an edge points from the first node to the second
+         node. Throw a KeyError if the first node wasn't in the graph."""
         try:
             return node2 in self.graph[node1]
         except KeyError:
             raise KeyError("{} doesn't exist".format(node1))
 
-    def depth_first_helper(self, node, visited):
+    def _depth_first_helper(self, node, visited):
         visited.append(node)
         for edge in self.graph[node]:
             if edge not in visited:
-                visited = self.depth_first_helper(edge, visited)
+                visited = self._depth_first_helper(edge, visited)
         return visited
 
     def depth_first_traversal(self, node):
-        return self.depth_first_helper(node, [])
+        """Perform a depth-first traversal of the graph. Return a list of
+         nodes in the order visited."""
+        return self._depth_first_helper(node, [])
 
     def breadth_first_traversal(self, node):
+        """Perform a breadth-first traversal of the graph. Return a list of
+         nodes in the order visited."""
         visited = []
         q = [node]
         while q:
