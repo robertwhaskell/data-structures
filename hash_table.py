ba86f5859fa8b0618f1a1696fa3ce0b8fafa1d69
Robert Haskell <robertwhaskell@gmail.com>
2015-03-14 17:37:39 -0700

    set up simple tests for hash_table

--- hash_table.py
+++ hash_table.py
@@ -1,3 +1,12 @@
 class Hash_Table(object):
-    def __init__(self):
-        self.hash_table = []
+    def __init__(self, size=10):
+        self.hash_table = [None] * size
+
+    def get(self, key):
+        pass
+
+    def set(self, key, val):
+        pass
+
+    def hash(self, key):
+        pass
--- test_hash_table.py
+++ test_hash_table.py
@@ -1,23 +1,39 @@
 import pytest
 from hash_table import Hash_Table
 
-
-def test_hash_init():
+@pytest.fixture(scope='function')
+def empty_hash():
     h = Hash_Table()
-    assert h.hash_table == []
+    return h
 
 
-def test_hash_table_of_fixed_size():
-    pass
+@pytest.fixture(scope='function')
+def populated_hash():
+    h = Hash_Table()
+    h.set('pearkey', 'pear')
+    h.set('orangekey', 'orange')
+    h.set('applekey', 'applekey')
+    return h
 
 
-def test_get():
-    pass
+def test_hash_init_empty():
+    h = Hash_Table()
+    assert h.hash_table == [None]
 
 
-def test_set():
-    pass
+def test_hash_init_with_table_size():
+    h = Hash_Table(1024)
+    assert len(h.hash_table) == 1024
 
 
-def test_hash():
-    pass
+def test_get(populated_hash):
+    assert populated_hash.get('pearkey') == 'pear'
+
+
+def test_set(empty_hash):
+    empty_hash.set('plumkey', 'plum')
+    assert empty_hash.get('plumkey') == 'plum'
+
+
+def test_hash(empty_hash):
+    assert empty_hash.hash('abc') == 294 % len(empty_hash.hash_table)
