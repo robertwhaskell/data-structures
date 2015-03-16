0df0ab2d5ed62b0e75b7db6d155493e54b5cfbc4
Robert Haskell <robertwhaskell@gmail.com>
2015-03-16 10:55:12 -0700

    completed tests. now to write the code.

--- test_insertion_sort.py
+++ test_insertion_sort.py
@@ -14,4 +14,12 @@ def test_insertion_sort_on_unordered_list():
 
 
 def test_insertion_sort_on_ordered_list():
-    assert insertion_sort([1, 2, 3, 4]) == [1, 2, 3, 4]
+    assert insertion_sort([1, 2, 2, 3, 4]) == [1, 2, 2, 3, 4]
+
+
+def test_insertion_sort_on_reverse_order_list():
+    assert insertion_sort([4, 3, 2, 1, 1]) == [1, 1, 2, 3, 4]
+
+
+def test_insertion_sort_strings():
+    assert insertion_sort(['a', 'd', 'b', 'c']) == ['a', 'b', 'c', 'd']
