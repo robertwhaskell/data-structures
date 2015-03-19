fe5eb15389c3c42739ff80ab277888367fedd63f
Robert Haskell <robertwhaskell@gmail.com>
2015-03-19 10:30:00 -0700

    doing a little cleanup.

--- quick_sort.py
+++ quick_sort.py
@@ -9,7 +9,6 @@ def _quick_sort_helper(array, lo, hi):
         partition = _partition(array, lo, hi)
         _quick_sort_helper(array, lo, (partition - 1))
         _quick_sort_helper(array, (partition + 1), hi)
-    return array
 
 
 def _partition(array, lo, hi):
@@ -28,18 +27,34 @@ def _partition(array, lo, hi):
 if __name__ == "__main__":
     from time import time
 
-    def worst_case(num):
-        l = range(num)
-        timer = time()
-        quick_sort(l)
-        return time() - timer
+    l = range(100000)
 
-    def best_case(num):
-        import random
-        l = random.sample(range(num), num)
-        timer = time()
-        quick_sort(l)
-        return time() - timer
+    timer = time()
+    quick_sort(l)
+    print "best case: "+str(time() - timer)
 
-    print "best case: "+str(best_case(1000))
-    print "worst case: "+str(worst_case(1000))
+    def shitty_quick_sort(array):
+        return _quick_sort_helper(array, 0, (len(array) - 1))
+
+    def shitty_quick_sort_helper(array, lo, hi):
+        if lo < hi:
+            partition = _partition(array, lo, hi)
+            _quick_sort_helper(array, lo, (partition - 1))
+            _quick_sort_helper(array, (partition + 1), hi)
+
+    def shitty_partition(array, lo, hi):
+        """same as non-shitty partition, but selects lowest index"""
+        pivot_index = hi
+        pivot_value = array[pivot_index]
+        array[pivot_index], array[hi] = array[hi], array[pivot_index]
+        stored_index = lo
+        for i in range(lo, hi + 1):
+            if array[i] < pivot_value:
+                array[i], array[stored_index] = array[stored_index], array[i]
+                stored_index += 1
+        array[stored_index], array[hi] = array[hi], array[stored_index]
+        return stored_index
+
+    timer = time()
+    shitty_quick_sort(l)
+    print "worst case: "+str(time() - timer)
