01ba552b7c5f1d1cb8f834ea524d159c89fa0448
Robert Haskell <robertwhaskell@gmail.com>
2015-03-19 11:17:33 -0700

    messing around.

--- radix_sort.py
+++ radix_sort.py
@@ -1,5 +1,17 @@
 def radix_sort(array):
-    pass
+
+    buckets = [[]] * 10
+    tens_place = -1
+
+    while True:
+        tens_place += 1
+        for val in array:
+            try:
+                buckets[int(str(val)[tens_place])].append(array.remove(val))
+            except IndexError:
+                pass
+
+        pass
 
 
 if __name__ == "__main__":
